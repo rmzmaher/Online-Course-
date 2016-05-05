edaf4f2be9624d58fadbdb99a31420813ffc9e7e
Ramez <rmz.aziz@gmail.com>
2016-05-05 14:52:22 +0200

    Rails_project v1 with test cases

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+require 'carrierwave'
+require 'acts_as_votable'
+require 'ckeditor'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
