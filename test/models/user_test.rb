2e3bebb9d82f9c8c43df46e092bd06c00dd628e5
Ramez <rmz.aziz@gmail.com>
2016-05-05 15:09:15 +0200

    Rails_project v2 with test cases

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,20 +4,4 @@ class UserTest < ActiveSupport::TestCase
    test "the truth" do
     	assert true
    end
-
-   test "valid user" do
-    assert (User.all.size==1) #inserted in fixtures
-    assert User.first.name="MyName"
-  end
-
-  test "invalid user same email exist" do
-    inv_user=User.new
-    inv_user.email= "hossam6@gmail.com"
-    inv_user.name= "Hossam"
-    inv_user.gender ="male"
-
-    assert inv_user.invalid?
-    assert inv_user.errors['email'].any?
-  end
-
 end
